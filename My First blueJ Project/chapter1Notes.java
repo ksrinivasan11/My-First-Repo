a2a11c81e56c7aaed38c9e366dca2a3fb01ff609
ksrinivasan11 <ksrinivasan@stu.naperville203.org>
2022-08-23 12:38:53 -0500

    Started BlueJ and ch1 notes

---
+++ My First blueJ Project/chapter1Notes.java
@@ -0,0 +1,46 @@
+
+/**
+ * Kaushik Srinivasan
+ * 8/23/22
+ * Notes for Ch 1
+ * Where we are going to take some code notes
+ * Collaborators: Mr. Hayes
+ */
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class chapter1Notes
+{
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        //basic data types
+        int x = 9, y, z;
+        float anum = 7;
+        double num = 5, num1 = 23.4567, num2;
+        char letter = 'd';
+        boolean choice = false;
+        long a = 9;
+        short b = 5;
+        
+        
+        //objects
+        String word = new String("Kaushik Srinivasan");
+        Integer number = new Integer(47);
+        Double number2 = new Double(34.5);
+        
+        
+        
+        
+        
+        
+    }
+}
